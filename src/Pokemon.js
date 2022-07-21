b712ce0f46df41749320eda22f702d5209a1bb15
RishabMandal <rishab829@gmail.com>
2022-07-21 19:33:19 +0530

    Dark mode in PokeDex

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -23,40 +23,59 @@ export default function Pokemon() {
     getData();
   });
 
+  // Dark Mode ( Click on PokeDex )
+  const [darkmode, setDarkmode] = useState("text-black");
+  function dark() {
+    if (darkmode === "white") {
+      setDarkmode("black");
+    } else {
+      setDarkmode("white");
+    }
+  }
+
   return (
     <>
-      <div className="block w-[600px] mx-auto mt-14 mb-96 rounded-lg border-8 border-red-500 text-white bg-red-600">
-        <div className="text-center text-2xl font-bold p-4">PokeDex</div>
-        <div className="justify-center mt-12 bg-gray-700 mx-auto rounded-lg w-[400px] border-8 border-white">
-          <img
-            src={pokemonimage}
-            className="bg-gray-700 mx-auto w-[200px]"
-            alt=""
-          />
+      <div className={`bg-${darkmode} w-100% py-9`}>
+        <div className="block w-[600px] mx-auto mt-14 mb-96 rounded-lg border-8 border-red-500 text-white bg-red-600">
+          <div
+            className={`text-center text-2xl text-${darkmode} font-bold p-4`}
+            onClick={dark}
+          >
+            PokeDex
+          </div>
+          <div className="justify-center mt-12 bg-gray-700 mx-auto rounded-lg w-[400px] border-8 border-white">
+            <img
+              src={pokemonimage}
+              className="bg-gray-700 mx-auto w-[200px]"
+              alt=""
+            />
+          </div>
+          <div className="mx-auto bg-gray-700 w-[400px] text-center mt-5 mb-2 rounded-lg">
+            <div className="text-2xl font-bold py-2 px-4">
+              Name : {pokemonname}
+            </div>
+            <div className="text-xl px-4 pb-4">Weight : {pokemonweight}kg</div>
+          </div>
+          <div className="flex justify-center">
+            <button
+              onClick={() => {
+                // New syntax
+                num !== 1 && setNum(--number);
+              }}
+              className="my-8 mx-3 px-6 pt-2 pb-3 bg-yellow-500 text-black text-lg hover:text-yellow-500 hover:bg-black rounded-xl"
+            >
+              Prev
+            </button>
+            <button
+              onClick={() => {
+                setNum(++number);
+              }}
+              className="my-8 mx-3 px-6 pt-2 pb-3 bg-yellow-500 text-black text-lg hover:text-yellow-500 hover:bg-black rounded-xl"
+            >
+              Next
+            </button>
+          </div>
         </div>
-        <div className="mx-auto bg-gray-700 w-[400px] text-center mt-5 mb-2 rounded-lg">
-          <div className="text-2xl font-bold py-2 px-4">Name : {pokemonname}</div>
-          <div className="text-xl px-4 pb-4">Weight : {pokemonweight}kg</div>
-        </div>
-        <div className="flex justify-center">
-        <button
-          onClick={() => {
-            // New syntax
-            num !== 1 && setNum(--number);
-          }}
-          className="my-8 mx-3 px-6 pt-2 pb-3 bg-yellow-500 text-black text-lg hover:text-yellow-500 hover:bg-black rounded-xl"
-        >
-          Prev
-        </button>
-        <button
-          onClick={() => {
-            setNum(++number);
-          }}
-          className="my-8 mx-3 px-6 pt-2 pb-3 bg-yellow-500 text-black text-lg hover:text-yellow-500 hover:bg-black rounded-xl"
-        >
-          Next
-        </button>
-      </div>
       </div>
 
       <div className="my-14 text-4xl font-bold text-center">PokeTab</div>
